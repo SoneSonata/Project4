3a4ec28b4b8fe44799a617cca4a9eb376ba27c7a
Ben Smith <bengsmith18@gmail.com>
2023-05-17 12:34:49 -0700

    too complex

--- j/main.js
+++ j/main.js
@@ -9,8 +9,6 @@ function init() {
     .then(response => response.json()) 
     .then(data => {
         arc = data;
-        let generations = [];
-        let generation = {};
         data.forEach(generation => {
             let gen = document.getElementById("gen");
             const option = document.createElement('OPTION');
@@ -24,7 +22,6 @@ function init() {
         console.log(gen.value);
         console.log(arc);
         const code = e.target.value;
-        let dd = document.getElementById("gen");
         arc.forEach(gen => {
             if(gen.code === code){
                 ind = gen;
@@ -42,38 +39,37 @@ function init() {
             const t1button = document.getElementById('t1');
     
             t1button.addEventListener('click', (e) => {
-    
-                let type1n = Math.floor(Math.random() * ind.types);
+                let type1n = ''; 
+                type1n = Math.floor(Math.random() * ind.types);
                 let ttext = document.querySelector('.type1');
-                let randtype1 = myTypes[type1n];
+                let randtype1 = "";
+                randtype1 = myTypes[type1n];
                 ttext.innerText = randtype1;
                 let typesize = 0
                 const pokebutton1 = document.getElementById("p1");
+                let typepool = [];
+                let filteredmon1;
+                let mymons1 = [];
                 
                 fetch("https://pokeapi.co/api/v2/type/" + randtype1)
                 .then(response => response.json()) 
                 .then(data =>{
-                    console.log(data.pokemon);
                     typesize = data.pokemon.length;
-                    console.log(typesize);
-                
+                    typepool = data.pokemon;
+                    mymons1 = filltype(typepool);
+                    console.log("compare",typepool,mymons1)
+                    filteredmon1 = filtermons(mymons1,ind.maxpkm);
                 });
                 
 
                 
                 pokebutton1.addEventListener('click', e=> {
-                    let rp1 = Math.floor(Math.random() * ind.maxpkm + 1);
-                    typecheck(rp1,nametype,ind.maxpkm);
-                    fetch("https://pokeapi.co/api/v2/pokemon/" + rp1)
+                    let rp1 = Math.floor(Math.random() * typesize);
+                    randpoke1 = typepool[rp1].pokemon.name;
+                    fetch("https://pokeapi.co/api/v2/pokemon/" + randpoke1)
                     .then(response => response.json()) 
                     .then(data => {
-                        let poketypes = [];
-                        data.types.forEach(type => {
-                            poketypes.push(type.type.name);
-                        });
-                        console.log(randtype1);
-                        console.log(poketypes);
-                        console.log(data);
+                        console.log(data.order);
                         console.log(data.species.name);
                         let start1name = document.getElementById("poke1");
                         start1name.textContent = data.species.name;
@@ -88,8 +84,6 @@ function init() {
 
                         
                     });
-                    console.log(ind.maxpkm,rp1);
-                    
                  });
                 
     
@@ -105,15 +99,42 @@ function init() {
     
 }
 
-function generaterandpoke (max, type) {
-    let rp1 = Math.floor(Math.random() * max);
-    fetch("https://pokeapi.co/api/v2/pokemon/" + rp1)
+
+function filltype (mons,max){
+    let typemons = [];
+    let tr = {};
+    mons.forEach(mon => {
+        fetch("https://pokeapi.co/api/v2/pokemon/" + mon.pokemon.name)
         .then(response => response.json()) 
         .then(data => {
-            
-        })
+            typemons.push(data);
+        });
+    });
+    return typemons;
 }
 
+function filtermons (mons,max){
+    let genlist = [];
+    let i;
+    console.log("mon", mons);
+    mons.forEach(item => {
+        let place = {};
+        place = item;
+        if(place.id <= max){
+            genlist.push(place);
+        }
+    });
+    // for(i = 0; i < mons.length; i++)
+    // {
+    //     console.log(mons[i].id)
+    //     if(mons[i].id <= max){
+    //         filt = mons[i];
+    //         genlist.push(filt);
+    //     }
+    // }
+    console.log(genlist);
+    return genlist;
+}
 // function typecheck (dex,randtype, max) {
 //     fetch("https://pokeapi.co/api/v2/pokemon/" + dex)
 //         .then(response => response.json()) 
